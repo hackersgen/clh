8130f5cd3febe4591e83fd71f5f4f3e470bc926d
Francesco Pastore <24416438+2h3ph3rd@users.noreply.github.com>
2025-10-28 10:23:23 +0100

    refactor(game): :sparkles: Use express instead of browser sync

--- clh-game/start.js
+++ clh-game/start.js
@@ -1,8 +1,18 @@
+const express = require("express");
 const compression = require("compression");
-const bs = require("browser-sync").create();
+const path = require("path");
 
-bs.init({
-    server: "./",
-    watch: true,
-    middleware: [compression({filter: () => true })]
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(compression());
+
+app.use(express.static(path.join(__dirname, ".")));
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ App running on http://localhost:${PORT}`);
 });
